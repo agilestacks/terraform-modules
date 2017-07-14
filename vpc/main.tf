d1d7e695bbe9541a14d228bc7e27bdb01eb6df03
Antons Kranga <akranga@gmail.com>
2017-07-14 18:38:43 +0300

    tuning cidr parameters

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public" {
     vpc_id = "${aws_vpc.main.id}"
     map_public_ip_on_launch = "${var.assign_public_ip}"
     availability_zone = "${var.availability_zone}"
-    cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 4, 0)}"
+    cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, 1)}"
     tags {
         Name = "${var.name}"
     }
