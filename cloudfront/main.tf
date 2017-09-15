3ae198467b820106d2e9cc85e2d93293370376fa
Antons Kranga <anton@agilestacks.com>
2017-09-16 00:59:06 +0300

    TLSv1

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -59,6 +59,7 @@ resource "aws_cloudfront_distribution" "main" {
     cloudfront_default_certificate = true
     acm_certificate_arn = "${var.acm_certificate_arn}"
     ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1"
   }
 }
 
