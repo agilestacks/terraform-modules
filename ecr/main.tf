d0b3f3ab0a8066fcfd951625c45444888815a820
Antons Kranga <akranga@gmail.com>
2017-11-22 20:17:59 +0200

    explicit depencency between ecr and poicy to solve `does not have attribute 'name' for variable 'aws_ecr_repository.main.name'`

--- ecr/main.tf
+++ ecr/main.tf
@@ -2,7 +2,9 @@ resource "aws_ecr_repository" "main" {
   name = "${var.name}"
 }
 
-resource "aws_ecr_repository_policy" "foopolicy" {
-  repository = "${aws_ecr_repository.main.name}"
+resource "aws_ecr_repository_policy" "main" {
+  repository = "${var.name}"
   policy = "${var.policy}"
+
+  depends_on = ["aws_ecr_repository.main"]
 }
